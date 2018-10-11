60438287e241568dee6e9685d5b5eaf8e4c91288
marjoven <81245995@qq.com>
2018-10-11 14:17:39 +0800

    up

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import utiDate from './lib/utiDate.js';
-
+import cookie from './lib/cookie.js';
+import dataFormat from './lib/dataFormat.js';
+import download from './lib/download.js';
+import validate from './lib/validate.js';
 export {
-  utiDate
-
+  utiDate,
+  cookie,
+  dataFormat,
+  validate,
+  download
 };
